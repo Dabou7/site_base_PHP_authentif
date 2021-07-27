16ba29481d2ae7de9d5afb1072966cefbc618ed9
Esther-Itam <esther.itam@live.fr>
2021-07-27 15:00:09 +0200

    push

---
+++ php/db/db.php
@@ -0,0 +1,9 @@
+<?php 
+
+$pdo = new PDO(
+    'mysql:host=185.98.131.149;dbname=esthe1541046;charset=utf8',
+    'esthe1541046',
+    'xzkdxwzrrn',
+      [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+  );
+  ?>
